1c46eabea9a9f03357d813e3eb1b1a88e8c12bf3
wangsfx <23020092204164@stu.xmu.edu.cn>
2019-07-24 21:02:32 +0800

    基础依赖和日志配置

---
+++ src/main/java/pers/wsf/study/netty/Main.java
@@ -0,0 +1,14 @@
+package pers.wsf.study.netty;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("Hello, world!");
+	}
+
+}
