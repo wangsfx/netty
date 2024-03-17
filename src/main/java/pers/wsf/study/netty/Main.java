8c3fad3c26e4792403a16402c1593b1ee8185cb4
wangsfx <wangsf.office@foxmail.com>
2024-03-17 15:25:32 +0800

    测试

--- src/main/java/pers/wsf/study/netty/Main.java
+++ src/main/java/pers/wsf/study/netty/Main.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2024 wangsf All Rights Reserved
+ */
+
 package pers.wsf.study.netty;
 
 import org.slf4j.Logger;
@@ -5,10 +9,10 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-	public static void main(String[] args) {
-		LOGGER.info("Hello, world!");
-	}
+  public static void main(String[] args) {
+    LOGGER.info("Hello, netty!");
+  }
 
 }
